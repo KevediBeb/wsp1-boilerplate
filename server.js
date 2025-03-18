e7564cad9d9df31ca301a94516468b36ac65cdb6
Kevin Törneus <118818940+KevediBeb@users.noreply.github.com>
2025-03-18 11:07:22 +0100

    boiler doiler

--- server.js
+++ server.js
@@ -1,22 +1,70 @@
 import "dotenv/config"
 import express from "express"
 import nunjucks from "nunjucks"
+import logger from "morgan"
+import bcrypt from "bcrypt"
+import session from "express-session"
+import bodyParser from "body-parser"
+import pool from "db.js"
+
 
 const app = express()
 const port = 3000
+
+
+const saltRounds = 10;
+const myPlaintextPassword = 'test';
+const someOtherPlaintextPassword = 'not_bacon';
+
+bcrypt.hash(myPlaintextPassword, 10, function(err, hash) {
+	// här får vi nu tag i lösenordets hash i variabeln hash
+	console.log(hash)
+})
+
 
 nunjucks.configure("views", {
   autoescape: true,
   express: app,
 })
 
+
 app.use(express.static("public"))
 
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+app.use(logger("dev"))
+
+app.use(session({
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { sameSite: true }
+}))
+
+
 app.get("/", (req, res) => {
+  if (req.session.views) {
+    req.session.views++
+  } else {
+    req.session.views = 1
+  }
   res.render("index.njk",
-    { title: "Qvixter", message: "Best service, legit." }
+    { title: "Test", message: "Funkar?", views: req.session.views }
   )
 })
+
+app.post('/', async (req, res) => {
+  console.log(req.body)
+  const {name, password} = req.body
+  const [result] = await pool
+    .promise()
+    .query(`SELECT * FROM user WHERE user.name = user.name`)
+  
+  console.log(result)
+  res.redirect("/")
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
